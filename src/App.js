501fbe6a8ff60176d18d4f695b59f2b583337cbb
Josh <jos77marrion@gmail.com>
2024-04-02 07:59:44 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 
 import "./App.css"
-import MessageView from "./message-view";
 import MessageList from "./message-list";
 class App extends Component {
     render() {
--- src/index.js
+++ src/index.js
@@ -2,13 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from "./App";
-// const name = {
-//     first: "James",
-//     second: "Love"
-// }
-// const element =
-//     <div>
-//         <p>Hello this is {name.first} and {name.second} are equal = {name.first.length + name.second.length}</p>
-//     </div>;
 
 ReactDOM.render(<App/>, document.getElementById('root'));
